553dc3517fbd1d10dfe51bd7678f596a54ffc65f
S-John-S <classworkpc@gmail.com>
2017-03-17 19:08:11 +0530

    Update BellmanFord.cpp

--- BellmanFord.cpp
+++ BellmanFord.cpp
@@ -5,3 +5,19 @@ struct Hako
 {    int saisho , saigo, omosa ;
 };
 void Function(const Hako ArEdge[], int, int[], int, int);
+int main() {
+    ios_base::sync_with_stdio(0);
+    // N =no of nodes, E=edge
+    int N,E; cin>> N >> E; 
+    int saisho; cin >> saisho;
+    Hako ArEdge[E];
+    int answer[N]; 
+    //to store min omosa (weight)
+
+    for(int i=0; i<E; i++) {
+        cin>> ArEdge[i].saisho >> ArEdge[i].saigo >> ArEdge[i].omosa;
+    }
+
+    Function(ArEdge, E, answer, N, saisho);
+    return 0;
+}
